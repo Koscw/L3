a6383d9900fae26a1e6c847c81e33b34b19d24c8
yaroslav <yaroslavfgsf@gmail.com>
2020-12-20 11:58:58 +0200

    add HW3RECTANGLES.java

--- src/HW3RECTANGLES.java
+++ src/HW3RECTANGLES.java
@@ -1,55 +1,65 @@
-public class HW3RECTANGLES {
+public class Hw3Rectangles {
 
-        int top;
-        int left;
-        int width;
-        int height;
-        public HW3RECTANGLES(int top){
-            this.top=top;
+    int top;
+    int left;
+    int width;
+    int height;
+
+    public Hw3Rectangles(int top) {
+        this.top = top;
     }
-    public int getTop(){
-            return top;
+
+    public int getTop() {
+        return top;
     }
-    public int getLeft(){
+
+    public int getLeft() {
         return left;
     }
-    public int getWidth(){
+
+    public int getWidth() {
         return width;
     }
-    public int getHeight(){
+
+    public int getHeight() {
         return height;
     }
-    public void setTop(int top){
-            this.top=top;
-    }
-    public void setWidth(int width){
-        this.width=width;
+
+    public void setTop(int top) {
+        this.top = top;
     }
 
-    public void setLeft(int left){
-        this.left=left;
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
     }
 
 
-    public HW3RECTANGLES(int top,int left){
-        this.top=top;
-        this.left=left;
-        this.height=0;
-        this.width=0;
+    public Hw3Rectangles(int top, int left) {
+        this.top = top;
+        this.left = left;
+        this.height = 0;
+        this.width = 0;
     }
-    public HW3RECTANGLES(int top,int left, int width){
-        this.top=top;
-        this.left=left;
-        this.width=width;
-        this.height=width;
+
+    public Hw3Rectangles(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = width;
     }
-    public HW3RECTANGLES(int top,int left, int width,int height){
-        this.top=top;
-        this.left=left;
-        this.width=width;
-        this.height=height;
+
+    public Hw3Rectangles(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
     }
-    public HW3RECTANGLES(HW3RECTANGLES rectangles){
+
+    public Hw3Rectangles(Hw3Rectangles rectangles) {
         this.top = rectangles.top;
         this.left = rectangles.left;
         this.width = rectangles.width;
